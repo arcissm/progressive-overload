79e7daa123cb4d6ed2149c30db33eddfa504df37
arcissm <myth.of.the.universe@gmail.com>
2024-12-01 12:59:35 -0500

    Week starts on Monday

--- src/ui/react/components/calendar/WeekDayHeader.tsx
+++ src/ui/react/components/calendar/WeekDayHeader.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const WeekDayHeader: React.FC = () => {
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  // Adjust the days of the week to start with Monday
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   return (
     <div className="calendar-grid">
